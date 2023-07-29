be4558a21f5109c53e07ef3f736a93864d15ae5b
Ka0lee <pekochan90@gmail.com>
2023-07-30 08:19:40 +0900

    first commit

---
+++ test/controllers/photographs_controller_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class PhotographsControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get photographs_index_url
+    assert_response :success
+  end
+
+end
