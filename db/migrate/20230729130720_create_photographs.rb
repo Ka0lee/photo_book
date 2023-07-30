0ebd99fd204e4ba9169fe54cfb27b749b0a75970
Ka0lee <pekochan90@gmail.com>
2023-07-30 10:17:24 +0900

    モデルの作成

---
+++ db/migrate/20230729130720_create_photographs.rb
@@ -0,0 +1,12 @@
+class CreatePhotographs < ActiveRecord::Migration[6.0]
+  def change
+    create_table :photographs do |t|
+      t.string     :title,               null: false
+      t.string     :info
+      t.date       :date,                null: false
+      t.integer    :category_id,         null: false,foreign_key: true
+      t.string     :user                 null: false,foreign_key: true
+      t.timestamps
+    end
+  end
+end
