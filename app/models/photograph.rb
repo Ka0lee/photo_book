4f2f4e692f61581f6c29d8637406e255af94dafe
Ka0lee <pekochan90@gmail.com>
2023-08-09 03:36:14 +0900

    アソシエーション修正

--- app/models/photograph.rb
+++ app/models/photograph.rb
@@ -5,4 +5,10 @@ class Photograph < ApplicationRecord
   validates :title, presence: true
   validates :date, presence: true
   validates :images, length: { minimum: 1, maximum: 5, message: "は1枚以上5枚以下にしてください" }
+
+    
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
+
 end
