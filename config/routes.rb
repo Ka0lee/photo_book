da783cfa2a8dd11f63d59deee9cabb623b428d7b
Ka0lee <pekochan90@gmail.com>
2023-08-11 16:17:44 +0900

    詳細ページルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'photographs#index'
   resources :users, only: [:edit, :update]
-  resources :photographs, only: [:index, :new, :create, :show]
+  resources :photographs, only: [:index, :new, :create, :show, :edit]
 end
