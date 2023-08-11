cfb484dffdaff32ad841c75a2cf34f25293fed78
Ka0lee <pekochan90@gmail.com>
2023-08-11 16:43:38 +0900

    カラムの型修正

--- db/migrate/20230729130720_create_photographs.rb
+++ db/migrate/20230729130720_create_photographs.rb
@@ -2,7 +2,7 @@ class CreatePhotographs < ActiveRecord::Migration[6.0]
   def change
     create_table :photographs do |t|
       t.string     :title,               null: false
-      t.string     :diary
+      t.text       :diary
       t.date       :date,                null: false
       t.integer    :category_id,         null: false
       t.references :user,                null: false,foreign_key: true
