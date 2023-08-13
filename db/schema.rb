0d4b85a516cfec3c358eca963117d173edb4eeca
Ka0lee <pekochan90@gmail.com>
2023-08-13 16:46:05 +0900

    diaryカラム名変更

--- db/schema.rb
+++ db/schema.rb
@@ -38,7 +38,7 @@ ActiveRecord::Schema.define(version: 2023_08_04_082820) do
     t.date "date", null: false
     t.integer "category_id", null: false
     t.bigint "user_id", null: false
-    t.text "diary"
+    t.text "daybook"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_photographs_on_user_id"
