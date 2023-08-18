476381dfb42cb4f53c3205d00e0af5f90b524828
Ka0lee <pekochan90@gmail.com>
2023-08-18 21:56:39 +0900

    マイグレーション変更２

--- db/schema.rb
+++ db/schema.rb
@@ -33,15 +33,6 @@ ActiveRecord::Schema.define(version: 2023_08_04_082820) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "photographs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "title", null: false
-    t.date "date", null: false
-    t.integer "category_id", null: false
-    t.text "daybook"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
