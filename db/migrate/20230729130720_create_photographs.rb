dd6d4b0e8379b1c5363199b1c67411d27f2fbd43
Ka0lee <pekochan90@gmail.com>
2023-08-18 21:42:59 +0900

    マイグレーション変更

--- db/migrate/20230729130720_create_photographs.rb
+++ db/migrate/20230729130720_create_photographs.rb
@@ -4,7 +4,6 @@ class CreatePhotographs < ActiveRecord::Migration[6.0]
       t.string     :title,               null: false
       t.date       :date,                null: false
       t.integer    :category_id,         null: false
-      t.references :user,                null: false,foreign_key: true
       t.text       :daybook
 
 
