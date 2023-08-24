f5f675befdb8e9808a8bf3d54f16f75da3f2475e
Ka0lee <pekochan90@gmail.com>
2023-08-25 02:56:47 +0900

    画像表示に関するヘルパーメソッド追加

--- app/helpers/photographs_helper.rb
+++ app/helpers/photographs_helper.rb
@@ -1,2 +1,9 @@
 module PhotographsHelper
+  def photograph_lists(photographs)
+    html = ''
+    photographs.each do |photograph|
+      html += render(partial: 'photograph',locals: { photograph: photograph })
+    end
+    return raw(html)
+  end
 end
