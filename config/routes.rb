27326593c6563544622614652242c42f1c4eb3a1
Ka0lee <pekochan90@gmail.com>
2023-08-31 21:47:23 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'photographs#index'
   resources :users, only: [:edit, :update]
-  resources :photographs, only: [:index, :new, :create, :show, :edit] do
+  resources :photographs do
    collection do
     get 'search'
+   end
   end
-end
 end
